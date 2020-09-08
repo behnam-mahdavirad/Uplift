050cfd6ad982aac21a483a1f4939548a69a94a38
Behnam Mahdavirad <behnam.mahdavirad@gmail.com>
2020-09-08 12:09:48 +0430

    Added Repos

---
+++ Uplift.DataAccess/Data/Repository/IRepository/IUnitOfWork.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Uplift.DataAccess.Repository.IRepository
+{
+    interface IUnitOfWork : IDisposable
+    {
+        ICategoryRepository Category { get; }
+        void Save();
+    }
+}
---
+++ Uplift.DataAccess/Data/Repository/UnitOfWork.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Uplift.DataAccess.Data;
+using Uplift.DataAccess.Repository.IRepository;
+
+namespace Uplift.DataAccess.Repository
+{
+    class UnitOfWork : IUnitOfWork
+    {
+        private readonly ApplicationDbContext _db;
+
+        public UnitOfWork(ApplicationDbContext db)
+        {
+            _db = db;
+            Category = new CategoryRepository(_db);
+        }
+
+        public ICategoryRepository Category { get; set; }
+
+        public void Dispose()
+        {
+            _db.Dispose();
+        }
+
+        public void Save()
+        {
+            _db.SaveChanges();
+        }
+    }
+}
